a476ec045f9fe910b11084a8090cd51b0b908103
msaio <nq2uit@gmail.com>
2025-06-06 14:12:04 +0700

    [msaio][neotree][keymaps] - Update `NeoTree reveal` keymaps
    Hit `<leader> + \` to toggle Neotree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,13 +11,15 @@ return {
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    -- { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          -- ['\\'] = 'close_window',
+          ['<leader>\\'] = 'close_window',
         },
       },
     },
