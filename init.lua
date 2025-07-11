7233a984bf98e498d9252c56da4c7ed1ec2b1b34
msaio <nq2uit@gmail.com>
2025-07-11 16:02:14 +0700

    [msaio][vim-obsession][keymaps] - <leader>to to toggle Track/Pause ; <leader>tO to toggle Track/Delete

--- init.lua
+++ init.lua
@@ -208,6 +208,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Session management keymaps (vim-obsession)
+vim.keymap.set('n', '<leader>to', '<cmd>Obsess<CR>', { desc = 'Toggle Track/Pause session' })
+vim.keymap.set('n', '<leader>tO', '<cmd>Obsess!<CR>', { desc = 'Toggle Track/Delete session' })
+
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
